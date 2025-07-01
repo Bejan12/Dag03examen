2733e3de094b2b5935cba449e37ee0d0ad58c595
Bejan Afkar <336498@student.mboutrecht.nl>
2025-07-01 10:32:12 +0200

    Complete VoedselbankMaaskantje project - Updated config and header with correct database name and Bootstrap navbar

--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,7 @@
  * De database verbindingsgegevens
  */
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'dag03examen');
+define('DB_NAME', 'VoedselbankMaaskantje');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 
@@ -11,7 +11,7 @@ define('DB_PASS', '');
 /**
  * De naam van de virtualhost
  */
-define('URLROOT', 'http://dag03examen/');
+define('URLROOT', 'http://dag03examen');
 
 /**
  * Het pad naar de folder app
